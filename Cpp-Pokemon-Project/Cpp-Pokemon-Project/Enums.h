fa70d9f3e2a6a109ec3ca11230329cb33f1eb190
Chance <aday031@naver.com>
2025-09-04 14:39:18 +0900

    Edit Enums

--- Cpp-Pokemon-Project/Cpp-Pokemon-Project/Enums.h
+++ Cpp-Pokemon-Project/Cpp-Pokemon-Project/Enums.h
@@ -23,12 +23,80 @@ enum class Type {
     FAIRY       // 요정
 };
 
+// 포켓몬의 능력치 종류
+enum class Stat {
+    HP,
+    ATTACK,
+    DEFENSE,
+    SPECIAL_ATTACK,
+    SPECIAL_DEFENSE,
+    SPEED
+};
+
+// 포켓몬의 성격
+enum class Nature {
+    HARDY,      // 노력 
+    LONELY,     // 외로움
+    ADAMANT,    // 고집
+    NAUGHTY,    // 개구쟁이
+    BRAVE,      // 용감
+    BOLD,       // 대담
+    DOCILE,     // 온순
+    IMPISH,     // 장난꾸러기
+    LAX,        // 촐랑
+    RELAXED,    // 무사태평
+    MODEST,     // 조심
+    MILD,       // 의젓
+    BASHFUL,    // 수줍음
+    RASH,       // 덜렁
+    QUIET,      // 냉정
+    CALM,       // 차분
+    GENTLE,     // 얌전
+    CAREFUL,    // 신중
+    QUIRKY,     // 변덕
+    SASSY,      // 건방
+    TIMID,      // 겁쟁이
+    HASTY,      // 성급
+    JOLLY,      // 명랑
+    NAIVE,      // 천진난만
+    SERIOUS     // 성실
+};
+
+// 경험치 그룹 (성장 속도)
+enum class ExpGroup {
+    ERRATIC,            // 600,000      (불규칙)
+    FAST,               // 800,000      (빠름)
+    MEDIUM_FAST,        // 1,000,000    (중간 빠름)
+    MEDIUM_SLOW,        // 1,059,860    (중간 느림)
+    SLOW,               // 1,250,000    (느림)
+    FLUCTUATING         // 1,640,000    (변동)
+};
+
 // 기술 타입
 enum class SkillCategory {
-    NOEN,
     PHYSICAL,   // 물리
     SPECIAL,    // 특수
     STATUS      // 변화
+};
+
+// 기술의 대상 범위
+enum class TargetType {
+    SINGLE_OPPONENT,    // 상대 하나
+    ALL_OPPONENTS,      // 인접한 모든 상대
+    SELF,               // 자기 자신
+    ALL_ALLIES,         // 모든 아군
+    ENTIRE_FIELD        // 필드 전체
+};
+
+
+// 기술의 특수 행동 방식
+enum class SkillBehavior {
+    NORMAL,
+    TWO_TURN_INVULNERABLE,      // 공중날기, 구멍파기처럼 1턴째에 피하는 기술
+    CHARGE_NO_INVULNERABLE,     // 솔라빔처럼 1턴째에 기를 모으는 기술
+    DIGGING,                    // 땅속에 숨은 상태
+    FLYING,                     // 공중에 있는 상태
+    // ... 등등
 };
 
 // 상태 이상
@@ -51,11 +119,20 @@ enum class VolatileStatus {
     FLINCH        // 풀죽음
 };
 
-enum class SkillBehavior {
-    NORMAL,
-    TWO_TURN_INVULNERABLE,      // 공중날기, 구멍파기처럼 1턴째에 피하는 기술
-    CHARGE_NO_INVULNERABLE,     // 솔라빔처럼 1턴째에 기를 모으는 기술
-    DIGGING,                    // 땅속에 숨은 상태
-    FLYING,                     // 공중에 있는 상태
-    // ... 등등
+// 날씨 상태
+enum class Weather {
+    NONE,
+    HARSH_SUNLIGHT,     // 쾌청
+    RAIN,               // 비
+    SANDSTORM,          // 모래바람
+    SNOW                // 설경
+};
+
+// 필드 상태
+enum class Terrain {
+    NONE,
+    ELECTRIC,           // 일렉트릭필드
+    GRASSY,             // 그래스필드
+    MISTY,              // 미스트필드
+    PSYCHIC             // 사이코필드
 };
