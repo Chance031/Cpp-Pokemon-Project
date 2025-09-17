91804296880ef2b0885a05cfe09978ddcbb0b2f0
Chance <aday031@naver.com>
2025-09-17 12:54:51 +0900

    Temp

---
+++ Cpp-Pokemon-Project/PokemonFireRed/GameTypes.h
@@ -0,0 +1,9 @@
+#pragma once
+
+enum class GameState
+{
+	Intro_GameFreak,      // 1. GAME FREAK 로고 장면
+	Intro_BattleAnim,     // 2. 겐가 vs 니드런 전투 장면
+	TitleScreen,          // 3. 타이틀 화면
+	InBattle              // 4. 인게임 전투 장면
+};
